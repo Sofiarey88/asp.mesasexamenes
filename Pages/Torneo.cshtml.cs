f2807be3ad03030abb966fd11ddf8a601dcc8a14
Sofiarey88 <sofiarey02@gmail.com>
2024-05-13 15:26:07 -0300

    Inicio del proyecto

---
+++ Pages/Torneo.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MesasExamenes.Pages
+{
+    public class TorneoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Proyecto ASP.NET Mesas de Exámenes
+## Cátedra: Base de datos II y Gestión de Proyectos de Software
+### 3er año-Técnico Superior en Desarrollo de Software
+              ***Año 2024***
